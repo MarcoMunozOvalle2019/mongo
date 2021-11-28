c487035932e3f75c8558fd0db357aa77e5eff267
MarcoMunozOvalle2019e <marco.munoz.ovalle2010@gmaile.com>
2021-11-28 15:39:56 -0300

    s agrega mongo

---
+++ src/app.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// connection to db
+//mongoose.connect('mongodb://mongo/database')
+mongoose.connect('mongodb+srv://juanPerez2022:juanitoperez2022@cluster0.4vvdi.mongodb.net/test')
+
+  .then(db => console.log('db connected'))
+  .catch(err => console.log(err));
+
+// importing routes
+const indexRoutes = require('./routes/index');
+
+// settings
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}))
+// routes
+app.use('/', indexRoutes);
+
+app.listen(app.get('port'), () => {
+  console.log(`server on port ${app.get('port')}`);
+});
